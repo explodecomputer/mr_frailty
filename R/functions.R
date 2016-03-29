f50c989f6b31da4132bc725f6c629c29468212c6
Gibran Hemani <explodecomputer@gmail.com>
2016-03-29 18:37:40 +0100

    first commit

---
+++ R/functions.R
@@ -0,0 +1,245 @@
+#' gompertz makeham cdf
+#'
+#' <full description>
+#'
+#' @param t <what param does>
+#' @param alpha=7.478359e-05 <what param does>
+#' @param beta=8.604875e-02 <what param does>
+#' @param lambda=-1.846973e-03 <what param does>
+#'
+#' @export
+#' @return vector
+gompertz_makeham_cdf <- function(t, alpha=7.478359e-05, beta=8.604875e-02, lambda=-1.846973e-03)
+{
+	pmin(pmax(1 - exp( -lambda * t - alpha / beta * (exp(beta*t) - 1) ), 0), 1)
+}
+
+
+#' simulate snps
+#'
+#' <full description>
+#'
+#' @param n <what param does>
+#' @param snp_freq <what param does>
+#'
+#' @export
+#' @return matrix
+simulate_snps <- function(n, snp_freq)
+{
+	snps <- sapply(snp_freq, function(x) rbinom(n, 2, x))
+	colnames(snps) <- paste0("snp", 1:ncol(snps))
+	return(snps)
+}
+
+
+#' simulate exposure
+#'
+#' <full description>
+#'
+#' @param n <what param does>
+#' @param snps <what param does>
+#' @param snp_beta <what param does>
+#' @param exposure_mean <what param does>
+#' @param exposure_sd <what param does>
+#'
+#' @export
+#' @return vector
+simulate_exposure <- function(n, snps, snp_beta, exposure_mean, exposure_sd)
+{
+	grs <- as.numeric(snps %*% snp_beta)
+	v_grs <- var(grs)
+	v_resid <- exposure_sd^2 - v_grs
+	grs <- scale(grs) * sqrt(v_grs / (v_grs + v_resid))
+	resid <- rnorm(n, 0, sqrt(v_resid / (v_grs + v_resid)))
+	exposure <- as.numeric(scale(grs + resid) * exposure_sd + exposure_mean)
+	return(exposure)
+}
+
+
+#' simulate events
+#'
+#' <full description>
+#'
+#' @param age <what param does>
+#' @param exposure <what param does>
+#' @param survival_function <what param does>
+#'
+#' @export
+#' @return vector
+simulate_events <- function(age, exposure, survival_function)
+{
+	n <- length(age)
+	survival <- survival_function(age, exposure)
+	event <- rbinom(n, 1, survival)
+	return(event)
+}
+
+
+#' plot quantiles
+#'
+#' <full description>
+#'
+#' @param exposure <what param does>
+#' @param outcome <what param does>
+#' @param exposure_breaks=10 <what param does>
+#' @param mediator=NULL <what param does>
+#' @param mediator_breaks=NULL <what param does>
+#' @param mediator_name=NULL <what param does>
+#' @param ylab="outcome" <what param does>
+#' @param xlab="exposure" <what param does>
+#'
+#' @export
+#' @return plot
+plot_quantiles <- function(exposure, outcome, exposure_breaks=10, mediator=NULL, mediator_breaks=NULL, mediator_name=NULL, ylab="outcome", xlab="exposure")
+{
+	require(dplyr)
+	require(ggplot2)
+	d <- data.frame(exposure=exposure, outcome=outcome, cuts=cut(exposure, breaks=exposure_breaks))
+
+	if(!is.null(mediator))
+	{
+		if(is.numeric(mediator_breaks))
+		{
+			d$mediator <- cut(mediator, mediator_breaks)
+		} else {
+			d$mediator <- mediator
+		}
+
+		res <- d %>% group_by(cuts, mediator) %>%
+			summarise(prob = mean(outcome), prob_sd = sd(outcome)/n())
+
+
+		p <- ggplot(res, aes(x=cuts, y=prob, colour=as.factor(mediator), group=as.factor(mediator))) +
+			geom_errorbar(aes(ymin=prob - prob_sd * 1.96, ymax=prob + prob_sd * 1.96), width=0) +	
+			stat_summary(fun.y=identity, geom="line") +
+			geom_point() +
+			theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5)) +
+			labs(x=xlab, y=ylab, colour=mediator_name) +
+			scale_colour_brewer(type="qual")
+
+	} else {
+
+		res <- d %>% group_by(cuts) %>%
+			summarise(prob = mean(outcome), prob_sd = sd(outcome)/n())
+
+		p <- ggplot(res, aes(x=cuts, y=prob)) +
+			geom_point() +
+			geom_errorbar(aes(ymin=prob - prob_sd * 1.96, ymax=prob + prob_sd * 1.96), width=0) +
+			theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5)) +
+			labs(x=xlab, y=ylab)
+
+	}
+	return(p)
+}
+
+
+
+
+#' pool mean sd
+#'
+#' <full description>
+#'
+#' @param n <what param does>
+#' @param m <what param does>
+#' @param s <what param does>
+#'
+#' @export
+#' @return data frame
+pool_mean_sd <- function(n, m, s)
+{
+	gn <- sum(n, na.rm=TRUE)
+	gm <- sum(m * n, na.rm=TRUE) / gn
+	gs <- sqrt(sum(n * (s^2 + (m - gm)^2), na.rm=TRUE) / gn)
+	return(data.frame(gn=gn, gm=gm, gs=gs))
+}
+
+
+#' simulate outcome binary
+#'
+#' DEPRECATED
+#'
+#' @param age_summary <what param does>
+#' @param max_age=100 <what param does>
+#' @param min_age=18 <what param does>
+#' @param sample_size_multiplier=3 <what param does>
+#'
+#' @export
+#' @return data frame
+simulate_outcome_binary <- function(age_summary, max_age=100, min_age=18, sample_size_multiplier=3)
+{
+	require(dplyr)
+	dat <- group_by(age_summary, cc) %>%
+		do({
+			data.frame(age = rnorm(.$gn * sample_size_multiplier, .$gm, .$gs), cc = .$cc, stringsAsFactors=FALSE)
+		}) %>%
+		as.data.frame()
+	dat$age <- pmin(max_age, dat$age) %>% pmax(min_age)
+	return(dat)
+}
+
+
+#' simulate ages
+#'
+#' <full description>
+#'
+#' @param n <what param does>
+#' @param age_mean <what param does>
+#' @param age_sd <what param does>
+#' @param max_age=100 <what param does>
+#' @param min_age=18 <what param does>
+#' @param sample_size_multiplier=3 <what param does>
+#'
+#' @export
+#' @return data frame
+simulate_ages <- function(n, age_mean, age_sd, max_age=100, min_age=18, sample_size_multiplier=3)
+{
+	require(Runuran)
+	dat <- data.frame(age = urnorm(n * sample_size_multiplier, age_mean, age_sd, ub=max_age, lb=min_age), stringsAsFactors=FALSE)
+	dat$age <- pmin(max_age, dat$age) %>% pmax(min_age)
+	return(dat)
+}
+
+
+#' get age summary
+#'
+#' <full description>
+#'
+#' @param demog <what param does>
+#' @param ncases <what param does>
+#' @param ncontrol <what param does>
+#' @param case_mean <what param does>
+#' @param control_mean <what param does>
+#' @param case_sd <what param does>
+#' @param control_sd <what param does>
+#'
+#' @export
+#' @return data frame
+get_age_summary <- function(demog, ncases, ncontrol, case_mean, control_mean, case_sd, control_sd)
+{
+	ages <- rbind(
+		pool_mean_sd(demog[[ncases]], demog[[case_mean]], demog[[case_sd]]),
+		pool_mean_sd(demog[[ncontrol]], demog[[control_mean]], demog[[control_sd]])
+	)
+	ages <- rbind(ages, pool_mean_sd(ages$gn, ages$gm, ages$gs))
+	ages$cc <- c(1,0,NA)
+	return(ages)
+}
+
+
+#' sample cases controls
+#'
+#' <full description>
+#'
+#' @param dat <what param does>
+#' @param age_summary <what param does>
+#'
+#' @export
+#' @return data frame
+sample_cases_controls <- function(dat, age_summary)
+{
+	temp1 <- subset(dat, alive==1 & cc==1)
+	temp1 <- temp1[sample(1:nrow(temp1), age_summary$gn[age_summary$cc==1]), ]
+	temp0 <- subset(dat, alive==1 & cc==0)
+	temp0 <- temp0[sample(1:nrow(temp0), age_summary$gn[age_summary$cc==0]), ]
+	return(rbind(temp1, temp0))
+}
---
+++ tests/bmi_pd.R
@@ -0,0 +1,110 @@
+# exposure, outcome and mortality
+
+# exposure has no effect on outcome
+
+# exposure has an effect on mortality
+
+# outcome occurs based on age
+
+# mortality is a function of age and exposure
+
+
+
+# exposure = snp(s)
+
+# mortality = age + exposure
+
+# outcome = age
+
+
+# simulate population with snp(s)
+
+# give them ages from a specified age distribution
+
+
+# total_n
+# alive_n
+
+# snp_freq
+# snp_beta
+# snp_effect_allele
+
+
+
+pd_incidence <- function(age, ...)
+{
+	pd_inc <- read.table("data-raw/pd_incidence.txt", he=T)
+	pd_inc$age <- (pd_inc$age_low + pd_inc$age_upp) / 2
+	pd_inc$n <- pd_inc$personyears / ((pd_inc$age_upp + pd_inc$age_low) / 2)
+	pd_inc$p <- pd_inc$cases / pd_inc$n
+	mod <- lm(p ~ poly(age,4), weights=n, pd_inc)
+	pred <- predict(mod, data.frame(age=age))
+	pred[pred < 0] <- 0
+	return(pred)
+}
+
+
+bmi_survival <- function(age, bmi)
+{
+	hr_summary <- read.table("data-raw/bmi_hr.txt", he=T)
+	hr_summary$lhr <- log(hr_summary$hr)
+	hr_summary$lhr_se <- (log(hr_summary$ci_upp) - log(hr_summary$ci_low)) / 3.92
+	
+	l <- list()
+	for(i in 1:nrow(hr_summary))
+	{
+		l[[i]] <- data.frame(
+			bmi = seq(hr_summary$bmi_low[i], hr_summary$bmi_upp[i], length.out=hr_summary$n[i]),
+			hr = exp(rnorm(hr_summary$n[i], hr_summary$lhr[i], hr_summary$lhr_se[i]))
+		)
+	}
+	hr_summary <- bind_rows(l)
+
+	mod <- lm(hr ~ poly(bmi, 4), hr_summary)
+	newdat <- data.frame(bmi = bmi)
+	hr <- predict(mod, newdat)
+	survival <- (1 - gompertz_makeham_cdf(age)) ^ hr
+	return(survival)
+}
+
+
+# bmi_survival <- function(age, bmi)
+# {
+# 	hr <- rep(1, length(bmi))
+# 	hr[bmi > 27] <- 5
+# 	survival <- (1 - gompertz_makeham_cdf(age)) ^ hr
+# 	return(survival)
+# }
+
+
+
+demog <- read.csv("data-raw/pd_demographics.csv")
+ages <- get_age_summary(demog, "Cases", "Controls", "Case_age_mean", "Control_age_mean", "Case_age_sd", "Control_age_sd")
+bmi_snps <- read.table("data-raw/bmi_2015_clumped.txt", he=T)
+bmi_snps$b <- bmi_snps$b * 4.18
+bmi_snps_mean <- 25
+bmi_snps_sd <- 4.18
+
+
+nsim <- 3
+l1 <- list()
+l2 <- list()
+for (i in 1:nsim)
+{
+	message(i)
+
+	dat <- simulate_ages(ages$gn[3], ages$gm[3], ages$gs[3], max_age=100, min_age=40, sample_size_multiplier=4)
+	dat$cc <- simulate_events(dat$age, NULL, pd_incidence)
+	snps <- simulate_snps(nrow(dat), bmi_snps$Freq1.Hapmap)
+	dat$bmi <- simulate_exposure(nrow(dat), snps, bmi_snps$b, bmi_snps_mean, bmi_snps_sd)
+	dat$alive <- simulate_events(dat$age, dat$bmi, bmi_survival)
+	dat$grs <- snps %*% bmi_snps$b
+	s <- sample_cases_controls(dat, age_summary)
+
+	a <- summary(glm(cc ~ bmi, subset(s), family="binomial"))
+	b <- summary(glm(cc ~ grs, subset(s), family="binomial"))
+
+	l1[[i]] <- coefficients(a)[2,]
+	l2[[i]] <- coefficients(b)[2,]
+
+}
