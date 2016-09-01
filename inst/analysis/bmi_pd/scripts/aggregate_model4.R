1af58fbdf062272282f9f82dcaefa9995905ba96
Gibran Hemani <explodecomputer@gmail.com>
2016-09-01 22:12:37 +0100

    finished model4

---
+++ inst/analysis/bmi_pd/scripts/aggregate_model4.R
@@ -0,0 +1,23 @@
+library(dplyr)
+
+arguments <- commandArgs(T)
+splits <- 10
+outdir <- "~/repo/mr_frailty/inst/analysis/bmi_pd/scratch4"
+savefile <- "~/repo/mr_frailty/inst/analysis/bmi_pd/results/model4.RData"
+
+l <- list()
+for(i in 1:splits)
+{
+	cat(i, "\n")
+	filename <- paste0(outdir, "/results", i, ".RData")
+	if(file.exists(filename))
+	{
+		load(filename)
+		l[[i]] <- res
+	} else {
+		message("Missing ", filename)
+	}
+}
+
+res <- rbind_all(l)
+save(res, file = savefile)
