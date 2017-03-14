54ea78432fa401844f2367f83c2fa62a16e35e17
Gibran Hemani <explodecomputer@gmail.com>
2017-03-14 22:58:59 +0000

    added model6

---
+++ inst/analysis/bmi_pd/scripts/run_model6.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#PBS -N bmi_pd
+#PBS -o job_reports/bmi_pd-output
+#PBS -e job_reports/bmi_pd-error
+#PBS -l walltime=4:00:00
+#PBS -t 1-100
+#PBS -l nodes=1:ppn=2
+#PBS -S /bin/bash
+
+set -e
+
+echo "Running on ${HOSTNAME}"
+
+if [ -n "${1}" ]; then
+  echo "${1}"
+  PBS_ARRAYID=${1}
+fi
+
+i=${PBS_ARRAYID}
+splits=10
+outdir="${HOME}/repo/mr_frailty/inst/analysis/bmi_pd/scratch6"
+
+R --no-save --args ${i} ${splits} ${outdir} < ${HOME}/repo/mr_frailty/inst/analysis/bmi_pd/scripts/model6.R
+
