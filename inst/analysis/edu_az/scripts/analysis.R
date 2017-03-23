08e1cd278c10424395d28db25f6ca1f0727c5f90
Gibran Hemani <explodecomputer@gmail.com>
2017-03-23 22:26:44 +0000

    updated

--- inst/analysis/edu_az/scripts/analysis.R
+++ inst/analysis/edu_az/scripts/analysis.R
@@ -24,3 +24,52 @@ plot_quantiles(exposure = dat$dat$age, outcome = dat$dat$alive, ylab = "Survival
 
 plot_quantiles(exposure = dat$dat$age, outcome = dat$dat$alive, ylab = "Survival", xlab="Age", exposure_breaks=10, mediator= dat$dat$cc, mediator_name="AZ")
 
+
+
+## ---- setup ----
+
+library(ggplot2)
+library(dplyr)
+library(systemfit)
+library(Runuran)
+
+load("../results/model1.RData")
+res1 <- res
+res1$model <- "model1"
+res1$modname <- "Edu ~ SNP; best estimate"
+load("../results/model2.RData")
+res2 <- res
+res2$model <- "model2"
+res2$modname <- "Edu ~ SNP; exaggerated effect"
+
+res <- rbind(res1, res2)
+
+res_plot <- subset(res, test %in% c("2SLS", "Observational", "MR Egger", "Inverse variance weighted"))
+
+res_plot$test <- as.factor(res_plot$test)
+levels(res_plot$test) <- c("2SLS", "IVW", "MR Egger", "Obs assoc")
+
+
+## ---- method_comparisons ----
+
+mc_dat <- group_by(res_plot, test, model) %>% 
+	dplyr::summarise(
+		b=mean(beta), 
+		se=sd(beta)/sqrt(n()), 
+		modname=first(modname), 
+		tval=b / se, 
+		pval=2 * pt(abs(tval), n()-1, low=FALSE)
+)
+
+mc_dat <- group_by(res_plot, test, model) %>% do({print(t.test(.$beta))})
+
+ggplot(subset(res_plot, test != "2SLS"), aes(x=(beta))) +
+geom_density(aes(fill=modname), alpha=0.5) +
+geom_vline(data=subset(mc_dat, model %in% c("model4", "model5") & test != "2SLS"), aes(xintercept=(b), colour=modname)) +
+geom_vline(xintercept=0, linetype="dashed") +
+facet_grid(test ~ ., scale="free_y") +
+scale_fill_brewer(type="qual") +
+scale_colour_brewer(type="qual") +
+labs(x="Effect estimate", fill="Model", colour="Model")
+
+mc_dat
